a8fb29ce0d202a63d6e5660da2fe87e684cfcd11
Paul Mecklenburg <paul.mecklenburg@gmail.com>
2015-01-02 13:34:13 -0500

    Add a test exhibiting issue #1 (unexpected hole)

---
+++ reg-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+declare -A svg2nc_args
+svg2nc_args[parquetry]="-t -.01 -x .15 -c 008000:.25 -c 008080:.2 -d .125 -m .25"
+
+fail_count=0
+for name in ${!svg2nc_args[*]}; do
+	base=test/${name}
+	args=${svg2nc_args[${name}]}
+	set -xe
+	./svg2nc -v -f 12 -l .2 ${args} -n ${base}.ngc~ -p ${base}.ps~ ${base}.svg
+	set +xe
+	for type in ngc ps; do
+		diff ${base}.${type} ${base}.${type}~
+		if [ $? -ne 0 ]; then
+			echo "FAILURE: Found differences in ${base}.${type}"
+			fail_count=$((fail_count+1))
+		else
+			echo "PASS: ${base}.${type}"
+		fi
+	done
+done
+
+if [ ${fail_count} -ne 0 ]; then
+	echo "TESTS FAILED"
+else
+	echo "ALL TESTS PASSED"
+fi
